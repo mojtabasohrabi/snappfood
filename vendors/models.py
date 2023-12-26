c9f08c28ce2d45e38bc8200710f141c349a6a46f
mojtaba <mojtaba.sohrabi.1998@gmail.com>
2023-12-27 00:47:31 +0330

    feat: create vendors model

--- vendors/models.py
+++ vendors/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Vendor(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='نام فروشگاه'
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name="زمان شروع همکاری"
+    )
